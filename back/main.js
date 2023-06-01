4aa6806ed3e8e8eed4c01e5ac9b573ede9d294fd
charline <croyer@normandiewebschool.fr>
2023-06-01 23:03:50 +0200

    .

--- back/main.js
+++ back/main.js
@@ -5,7 +5,7 @@ import { imageRouter } from "./Controllers/ImageController.js";
 import cors from "cors";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
@@ -19,16 +19,28 @@ app.use(
 );
 
 // Connect to the MongoDB database
+// mongoose
+//   .connect("mongodb://localhost:27017/account-projet4-express")
+//   .then(() => console.log("Connected to MongoDB database"))
+//   .catch((error) =>
+//     console.log("Error connecting to MongoDB database: ", error)
+//   );
+
 mongoose
-  .connect("mongodb://localhost:27017/account-projet4-express")
-  .then(() => console.log("Connected to MongoDB database"))
-  .catch((error) =>
-    console.log("Error connecting to MongoDB database: ", error)
+  .connect(
+    "mongodb+srv://charline:Charline123@cluster0.o5elyoa.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("Connecté à la base de données MongoDB"))
+  .catch((erreur) =>
+    console.log(
+      "Erreur lors de la connexion à la base de données MongoDB : ",
+      erreur
+    )
   );
 
 // Start the Express server
 app.listen(PORT, () => {
-  console.log("Server listening on port 3000");
+  console.log("Server listening on port 3001");
 });
 
 app.use("/", accountRouter);
